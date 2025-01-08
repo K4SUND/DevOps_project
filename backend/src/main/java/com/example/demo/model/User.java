1d7668dd7bce46091b8a9bf1594b97bb7930f303
K4SUND <kasundilumina2016@gmail.com>
2025-01-08 10:34:18 +0530

    1234545

---
+++ backend/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+
+}
